8a73d07dc9e1b7c278bbeba7dbaec281d2e6198a
Rhys <rhys@rhyswynne.co.uk>
2015-08-15 14:12:16 +0100

    Pulls in and add a featured image should one not be present.

--- inc/api-connection.php
+++ inc/api-connection.php
@@ -1,31 +1,108 @@
 <?php
 
 
-define('CLIENT_ID', '603647fd1bed7658d61538e8d66293e8' );
-define('CLIENT_SECRET', '7fc36c961b2053215488a6ec2e2979b3' );
+define( 'CLIENT_ID', '603647fd1bed7658d61538e8d66293e8' );
+define( 'CLIENT_SECRET', '7fc36c961b2053215488a6ec2e2979b3' );
 define( 'TEST_TRACK', 'https://soundcloud.com/thenextweb/testing-rapmic-for-ios' );
+define( 'POST_TYPE', 'post' );
 
+
+/**
+ * Add track details when a post is saved.
+ *
+ * @param int 	$post_id 		The post ID.
+ * @param post 	$post 			The post object.
+ * @param bool 	$update 		Whether this is an existing post being updated or not.
+ */
+function soundpress_add_track_details_to_post( $post_id, $post, $update ) {
+
+	$trackurl = TEST_TRACK;
+
+	if ( wp_is_post_revision( $post_id ) ) {
+		return;
+	}
+
+	if ( $trackurl ) {
+
+		$track_details = soundcloud_remote_get( $trackurl );
+
+		if ( is_object( $track_details ) ) {
+
+			// Check for thumbnail. If not present, get the board Image
+			if ( !has_post_thumbnail( $post_id ) ) {
+
+				$thumbnailurl = $track_details->artwork_url;
+				
+				$tmp = download_url( $thumbnailurl );
+				
+				if( is_wp_error( $tmp ) ){
+
+				}
+				
+				$desc = get_the_title( $post_id );
+				$file_array = array();
+
+				// Set variables for storage
+				// fix file filename for query strings
+				preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $thumbnailurl, $matches);
+				$file_array['name'] = basename($matches[0]);
+				$file_array['tmp_name'] = $tmp;
+
+				// If error storing temporarily, unlink
+				if ( is_wp_error( $tmp ) ) {
+					@unlink($file_array['tmp_name']);
+					$file_array['tmp_name'] = '';
+				}
+
+				// do the validation and storage stuff
+				$id = media_handle_sideload( $file_array, $post_id, $desc );
+
+				// If error storing permanently, unlink
+				if ( is_wp_error($id) ) {
+					@unlink($file_array['tmp_name']);
+					return $id;
+				}
+
+				set_post_thumbnail( $post_id, $id );
+
+			}
+
+		}
+
+	}
+	
+}
+add_action( 'save_post_' . POST_TYPE, 'soundpress_add_track_details_to_post', 10, 3 );
 
 /**
  * Connect to a Soundcloud URL and then pull in details.
  *
- * @return void
+ * @param  string $trackurl 		 	The URL string.
+ * @return mixed 						Array if track found, or else false.
  */
-function soundcloud_remote_get() {
+function soundcloud_remote_get( $trackurl ) {
 
-	$trackname 		= soundcloud_get_track_name( TEST_TRACK );
+
+	$trackname 		= soundcloud_get_track_name( $trackurl );
 	$apiurl			= 'http://api.soundcloud.com/tracks/' . $trackname . '?client_id=' . CLIENT_ID;
 
-	$response = wp_remote_get( $apiurl, $args );
+	$response = wp_remote_get( $apiurl );
 
 	if( is_array($response) ) {
-  		$header 	= $response['headers']; // array of http header lines
-  		$body 		= $response['body']; // use the content
-	}
 
-	wp_die( print_r( $header ) . '<br/><br/>' . print_r( $body ) );
+  		$trackdetailsjson 		= $response['body']; // use the content
 
-}
+  		$trackdetails = json_decode( $trackdetailsjson );
+
+  		if ( is_object( $trackdetails ) ) {
+  			return $trackdetails;
+  		} else {
+  			return FALSE;
+  		}
+  	} else {
+  		return FALSE;
+  	}
+  } 
 
 
 /**
@@ -36,7 +113,7 @@ function soundcloud_remote_get() {
 function soundcloud_get_track_name( $url ) {
 
 	$urlparts 	= explode( '/', $url );
-	$trackname 	= end( $url );
+	$trackname 	= end( $urlparts );
 
 	return $trackname; 
 }
